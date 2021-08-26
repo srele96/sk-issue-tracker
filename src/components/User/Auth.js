298fe61eff90236c1427828975ba377d6a502d8b
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2021-08-26 14:29:55 +0200

    Feature - Auth redirect.
    User is redirected to page he initiated sign in from.

--- src/components/User/Auth.js
+++ src/components/User/Auth.js
@@ -1,16 +1,19 @@
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import { useLocation } from 'react-router-dom';
 import { auth } from '../../util';
 
 const uiConfig = {
   // Popup signin flow rather than redirect flow.
   signInFlow: 'popup',
-  callbacks: {
-    signInSuccessWithAuthResult: () => false,
-  },
   // We will display Google as auth provider.
   signInOptions: [auth.GoogleAuthProvider.PROVIDER_ID],
 };
 
-export const Auth = () => (
-  <StyledFirebaseAuth firebaseAuth={auth()} uiConfig={uiConfig} />
-);
+export const Auth = () => {
+  // Determine what page user is in, if he try to sign in,
+  // redirect him to page he tried to sign in from.
+  const { pathname } = useLocation();
+  uiConfig.signInSuccessUrl = pathname;
+
+  return <StyledFirebaseAuth firebaseAuth={auth()} uiConfig={uiConfig} />;
+};
